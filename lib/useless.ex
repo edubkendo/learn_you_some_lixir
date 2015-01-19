bc5d1c79c4e8f1c20e16787ab4b0c6a26eda5c39
Eric West <esw9999@gmail.com>
2015-01-19 05:01:47 -0600

    Fix spaces in useless.

--- lib/useless.ex
+++ lib/useless.ex
@@ -1,10 +1,10 @@
 defmodule Useless do
-	def add(a, b), do: a + b
+  def add(a, b), do: a + b
 
-	def hello(), do: IO.puts "Hello, world!"
+  def hello(), do: IO.puts "Hello, world!"
 
-	def greet_and_add_two(x) do
-		hello
-		add x, 2
-	end
+  def greet_and_add_two(x) do
+    hello
+    add x, 2
+  end
 end
