22754b72c1b40fc392bfba95353919099276d2fd
Eric West <esw9999@gmail.com>
2015-01-19 04:43:18 -0600

    tabs to spaces

--- lib/guards.ex
+++ lib/guards.ex
@@ -1,12 +1,12 @@
 defmodule Guards do
-	def old_enough(x) when x >= 16 do
-		true
-	end
-	def old_enough(_), do: false
+  def old_enough(x) when x >= 16 do
+    true
+  end
+  def old_enough(_), do: false
 
-	def right_age(x) when x >= 16 and x <= 104, do: true
-	def right_age(_), do: false
+  def right_age(x) when x >= 16 and x <= 104, do: true
+  def right_age(_), do: false
 
-	def wrong_age(x) when x < 16 or x > 104, do: true
-	def wrong_age(_), do: false
+  def wrong_age(x) when x < 16 or x > 104, do: true
+  def wrong_age(_), do: false
 end
--- lib/quick_sort.ex
+++ lib/quick_sort.ex
@@ -1,24 +1,24 @@
 defmodule QuickSort do
-	def quicksort([]), do: []
-	def quicksort([pivot|rest]) do
-	  {smaller, larger} = partition(pivot, rest, [], [])
-	  quicksort(smaller) ++ [pivot] ++ quicksort(larger)
-	end
+  def quicksort([]), do: []
+  def quicksort([pivot|rest]) do
+    {smaller, larger} = partition(pivot, rest, [], [])
+    quicksort(smaller) ++ [pivot] ++ quicksort(larger)
+  end
 
-	def partition(_, [], smaller, larger), do: {smaller, larger}
-	def partition(pivot, [h|t], smaller, larger) do
-		cond do
-			h <= pivot ->
-				partition(pivot, t, [h|smaller], larger)
-		  h > pivot ->
-				partition(pivot, t, smaller, [h|larger])
-		end
-	end
+  def partition(_, [], smaller, larger), do: {smaller, larger}
+  def partition(pivot, [h|t], smaller, larger) do
+    cond do
+      h <= pivot ->
+        partition(pivot, t, [h|smaller], larger)
+      h > pivot ->
+        partition(pivot, t, smaller, [h|larger])
+    end
+  end
 
-	def lc_quicksort([]), do: []
-	def lc_quicksort([pivot|rest]) do
-		lc_quicksort((for smaller <- rest, smaller <= pivot, do: smaller))
-		++ [pivot] ++
-			lc_quicksort((for larger <- rest, larger > pivot, do: larger))
-	end
+  def lc_quicksort([]), do: []
+  def lc_quicksort([pivot|rest]) do
+    lc_quicksort((for smaller <- rest, smaller <= pivot, do: smaller))
+    ++ [pivot] ++
+      lc_quicksort((for larger <- rest, larger > pivot, do: larger))
+  end
 end
